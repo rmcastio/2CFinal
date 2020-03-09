15965612a3990cf32132e6b05eba46700670426d
Richard Castillo <armed_deity@hotmail.com>
2020-03-09 04:16:42 -0700

    Working on elementID

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+const calClear = document.getElementById("button-clear");
+const calDisplay = document.getElementById("display")
+const calButtonPlus = document.getElementById("button-plus");
+const calButtonMinus = document.getElementById("button-minus");
+const calButtonMultiply = document.getElementById("button-multiply");
+const calButtonDivide = document.getElementById("button-divide");
+const calButtonEquals = document.getElementById("button-equals");
